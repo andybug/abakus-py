783f0a18fbae167fed2bf6cac1358a5486ba2586
Andrew Fields <andy@andybug.com>
2018-02-25 15:53:43 -0600

    Read snapshots

--- scripts/abakus.py
+++ scripts/abakus.py
@@ -188,6 +188,9 @@ class AbakusFileMetadataList:
     def list(self):
         return self.metadataList
 
+    def add(self, metadata):
+        self.metadataList.append(metadata)
+
 
 class AbakusBlobStore:
     def __init__(self, abakus, blobDir):
@@ -227,13 +230,16 @@ class AbakusMetadataStore:
     def add(self, metadata):
         if os.path.isfile(os.path.join(self.metadataDir, metadata.metadataHash)):
             logging.debug('%s (%s) already exists in MetadataStore, loading...' % (metadata.getShortMetadataHash(), metadata.relPath))
-            return self.__read(metadata)
+            return self.__read(metadata.metadataHash)
 
         self.abakus.blobStore.add(metadata)
         self.__write(metadata)
 
-    def __read(self, metadata):
-        metadataPath = os.path.join(self.metadataDir, metadata.metadataHash)
+    def load(self, metadataHash):
+        return self.__read(metadataHash)
+
+    def __read(self, metadataHash):
+        metadataPath = os.path.join(self.metadataDir, metadataHash)
         with open(metadataPath, 'rb') as f:
             metadataFile = yaml.load(str(zlib.decompress(f.read()), 'utf8'))
             if metadataFile['type'] != 'FileMetadata':
@@ -267,7 +273,7 @@ class AbakusSnapshotStore:
         obj['type'] = 'Snapshot'
         obj['version'] = 1
         obj['timestamp'] = int(time.time())
-        obj['metadatalist'] = []
+        obj['metadataList'] = []
 
         hash = blake2b(digest_size=32)
         hash.update(bytes(str(obj['timestamp']), 'utf8'))
@@ -275,15 +281,41 @@ class AbakusSnapshotStore:
         for metadata in metadataList.list():
             self.abakus.metadataStore.add(metadata)
             hash.update(bytearray(metadata.metadataHash, 'utf8'))
-            obj['metadatalist'].append(metadata.metadataHash)
+            obj['metadataList'].append(metadata.metadataHash)
 
-        self.__write(obj, hash.hexdigest())
+        obj['snapshotHash'] = hash.hexdigest()
 
-    def __write(self, obj, snapshotID):
-        with open(os.path.join(self.snapshotDir, snapshotID), 'wb') as f:
+        with open(os.path.join(self.snapshotDir, str(obj['timestamp'])), 'wb') as f:
             with io.StringIO() as stream:
                 yaml.dump(obj, stream, default_flow_style=False, indent=2)
                 f.write(zlib.compress(bytes(stream.getvalue(), 'utf8')))
+
+        logging.info('Wrote snapshot %d' % obj['timestamp'])
+
+    def latest(self):
+        sortedList = sorted(os.listdir(self.snapshotDir), key=lambda x: int(x), reverse=True)
+        if len(sortedList) == 0:
+            return None
+        logging.debug('Latest snapshot is %s' % sortedList[0])
+        return self.__readSnapshot(sortedList[0])
+
+    def __readSnapshot(self, timestamp):
+        snapshotFilePath = os.path.join(self.snapshotDir, timestamp)
+        with open(snapshotFilePath, 'rb') as f:
+            snapshotFile = yaml.load(str(zlib.decompress(f.read()), 'utf8'))
+            if snapshotFile['type'] != 'Snapshot':
+                logging.error('Expected type Snapshot: %s' % snapshotFilePath)
+                exit(1)
+            if snapshotFile['version'] != 1:
+                logging.error('Unknown Snapshot version %d: %s' % (snapshotFile['version'], snapshotFilePath))
+                exit(1)
+
+            list = AbakusFileMetadataList()
+            for metadata in snapshotFile['metadataList']:
+                list.add(self.abakus.metadataStore.load(metadata))
+
+            return list
+        return None
 
 
 class Abakus:
@@ -337,7 +369,7 @@ class Abakus:
             config = yaml.load(f)
 
     def cmd_status(self):
-        pass
+        print(self.snapshotStore.latest())
 
     def cmd_snapshot(self):
         workdir = AbakusFileMetadataList(dir=self.root)
