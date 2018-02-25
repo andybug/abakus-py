4f1e591e4d4756ad558fd80b1dc35837d7b46928
Andrew Fields <andy@andybug.com>
2018-02-25 16:35:33 -0600

    Add AbakusDiff

--- scripts/abakus.py
+++ scripts/abakus.py
@@ -192,6 +192,55 @@ class AbakusFileMetadataList:
         self.metadataList.append(metadata)
 
 
+class AbakusDiff:
+    def __init__(self, old, new):
+        self.added = []
+        self.removed = []
+        self.changed = []
+
+        oldMap = {}
+
+        for metadata in old.list():
+            oldMap[metadata.relPath] = (metadata, False)
+
+        for metadata in new.list():
+            if not metadata.relPath in oldMap:
+                self.added.append(metadata)
+                continue
+            (oldMetadata, _) = oldMap[metadata.relPath]
+            oldMap[metadata.relPath] = (oldMetadata, True)
+            if not oldMetadata.metadataHash == metadata.metadataHash:
+                self.changed.append(metadata)
+
+        for relPath, (metadata, inNew) in oldMap.items():
+            if not inNew:
+                self.removed.append(metadata)
+
+    def __str__(self):
+        with io.StringIO() as f:
+            if len(self.added) > 0:
+                f.write('Added:\n')
+                for a in self.added:
+                    f.write(str(a))
+                    f.write('\n')
+                f.write('\n')
+
+            if len(self.removed) > 0:
+                f.write('Removed:\n')
+                for a in self.removed:
+                    f.write(str(a))
+                    f.write('\n')
+                f.write('\n')
+
+            if len(self.changed) > 0:
+                f.write('Changed:\n')
+                for a in self.changed:
+                    f.write(str(a))
+                    f.write('\n')
+
+            return f.getvalue()
+
+
 class AbakusBlobStore:
     def __init__(self, abakus, blobDir):
         self.abakus = abakus
@@ -369,7 +418,10 @@ class Abakus:
             config = yaml.load(f)
 
     def cmd_status(self):
-        print(self.snapshotStore.latest())
+        latest = self.snapshotStore.latest()
+        workdir = AbakusFileMetadataList(dir=self.root)
+        diff = AbakusDiff(latest, workdir)
+        print(diff)
 
     def cmd_snapshot(self):
         workdir = AbakusFileMetadataList(dir=self.root)
